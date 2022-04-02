255fa1ff6624e8f680f08a8d4e006060d5fb99b9
WesMcW <wesmcwhorter@gmail.com>
2022-04-02 18:21:43 -0500

    Update WeatherMan.cs

--- maybeLemonadeStand/Assets/Scripts/WeatherMan.cs
+++ maybeLemonadeStand/Assets/Scripts/WeatherMan.cs
@@ -2,20 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeatherMan : MonoBehaviour
+public static class WeatherMan
 {
 
-
-    // Start is called before the first frame update
-    void Start()
+    public static Weather GetForcast(Season s)
     {
-        
-    }
+        Weather currentWeather;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float rand = Random.Range(0f, 1f);
+
+        switch (s)
+        {
+            //SPRING
+            case Season.Spring:
+                if (rand <= 0.67)
+                    currentWeather = Weather.Sun;
+                else if (rand > .67 && rand <= .85)
+                    currentWeather = Weather.Rain;
+                else if (rand > .85 && rand <= .98)
+                    currentWeather = Weather.Wind;
+                else
+                    currentWeather = Weather.Snow;
+                break;
+            //SUMMER
+            case Season.Summer:
+                if (rand <= 0.7)
+                    currentWeather = Weather.Sun;
+                else if (rand > .7 && rand <= .8)
+                    currentWeather = Weather.Rain;
+                else if (rand > .8 && rand <= 1)
+                    currentWeather = Weather.Wind;
+                else
+                    currentWeather = Weather.Snow;
+                break;
+            //FALL
+            case Season.Fall:
+                if (rand <= 0.67)
+                    currentWeather = Weather.Sun;
+                else if (rand > .67 && rand <= .8)
+                    currentWeather = Weather.Rain;
+                else if (rand > .8 && rand <= .98)
+                    currentWeather = Weather.Wind;
+                else
+                    currentWeather = Weather.Snow;
+                break;
+            //WINTER
+            case Season.Winter:
+                if (rand <= .2)
+                    currentWeather = Weather.Sun;
+                else if (rand > .2 && rand <= .23)
+                    currentWeather = Weather.Rain;
+                else if (rand > .23 && rand <= .33)
+                    currentWeather = Weather.Wind;
+                else
+                    currentWeather = Weather.Snow;
+                break;
+
+            default:
+                currentWeather = Weather.Snow;
+                break;
+        }
+
+        return currentWeather;
     }
 }
 
