75412638cd689f47965293721b6d670499ad8c65
WesMcW <wesmcwhorter@gmail.com>
2022-04-02 17:15:12 -0500

    sprites

--- maybeLemonadeStand/Assets/Scripts/WindAnim.cs
+++ maybeLemonadeStand/Assets/Scripts/WindAnim.cs
@@ -6,13 +6,23 @@ public class WindAnim : MonoBehaviour
 {
     Animator anim;
 
+    public Transform p1, p2;
+
+    public GameObject container;
+
     void Start()
     {
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         
+    }
+
+    public void ChooseNewPos()
+    {
+        var temp = Random.Range(p1.position.y, p2.position.y);
+        container.transform.position = new Vector3(container.transform.position.x, temp, container.transform.position.z);
     }
 }
