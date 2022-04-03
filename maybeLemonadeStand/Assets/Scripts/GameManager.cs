42beac74fb8097981e2334fe968e9a4145c41b69
Leah Blasczyk <scout1665@gmail.com>
2022-04-03 12:44:51 -0500

    making bank

--- maybeLemonadeStand/Assets/Scripts/GameManager.cs
+++ maybeLemonadeStand/Assets/Scripts/GameManager.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("Day Data")]
+    public Weather todaysForecast;
     public List<CartItem> purchasedIngredients;
     public List<Recipe> menuList;
+    float earnings = 7F;
 
     [Header("Day Menu Systems")]
+    public GameObject mainMenuPanel;
     public ShoppingCart shoppingSystem;
     public MenuSelector menuSystem;
+
+    [HideInInspector]
+    public UnityEvent OnBankChanged;
 
     [Header("Player Data")]
     public float bank = 7F;
     public int dayCounter = 7;
     public string truckName = "Seven";
+    public List<Upgrade> unlockedUpgrades;
 
     private void Awake()
     {
@@ -32,6 +41,9 @@ public class GameManager : MonoBehaviour
 
     public void StartNewDay()
     {
+        todaysForecast = WeatherMan.GetForcast(Calendar.GetSeason(dayCounter));
+        mainMenuPanel.SetActive(true);
+
         shoppingSystem.gameObject.SetActive(true);
         shoppingSystem.SetUpShop();
     }
@@ -39,6 +51,8 @@ public class GameManager : MonoBehaviour
     public void SendPurchases(float cost, List<CartItem> cart)
     {
         // money stuff
+        bank -= cost;
+        OnBankChanged.Invoke();
 
         purchasedIngredients = new List<CartItem>();
         foreach (var item in cart) purchasedIngredients.Add(item);
@@ -57,5 +71,28 @@ public class GameManager : MonoBehaviour
     {
         menuList = new List<Recipe>();
         foreach (var recipe in menu) menuList.Add(recipe);
+
+        StartDay();
+    }
+
+    public void StartDay()
+    {
+        if (ScreenFader.instance)
+        {
+            ScreenFader.instance.ScreenFade(ToTruckTransition);
+        }
+        else ToTruckTransition();
+        // start animation here
+        // send earnings to endofday ui
+    }
+
+    void ToTruckTransition()
+    {
+        menuSystem.gameObject.SetActive(false);
+        earnings = MoneyLogic.GetDailyEarnings(menuList, todaysForecast);
+        bank += earnings;
+        Debug.Log("MAKING BANK: " + bank);
+        OnBankChanged.Invoke();
+        mainMenuPanel.SetActive(false);
     }
 }
