9b6a7fc1c4190fb0775f57cd09248e7fb7917b59
Leah Blasczyk <scout1665@gmail.com>
2022-04-03 15:20:57 -0500

    end of day lookin nice

--- maybeLemonadeStand/Assets/Scripts/EndDayReport.cs
+++ maybeLemonadeStand/Assets/Scripts/EndDayReport.cs
@@ -12,17 +12,28 @@ public class EndDayReport : MonoBehaviour
     public TextMeshProUGUI totalTxt;
     public TextMeshProUGUI bankTxt;
 
+    public List<GameObject> weatherIcons;
+
     public void LoadEndDay(List<Recipe> menu, Weather forecast, float moneySpent) {
         titleTxt.text = "End of Day " + GameManager.instance.dayCounter.ToString();
+        SetWeatherIcon(forecast);
 
         var moneys = MoneyLogic.GetDailyEarnings(menu, forecast);
         float total = 0F;
 
-        for(int iterator = 0; iterator < menu.Count; iterator++)
+        for(int iterator = 0; iterator < 3; iterator++)
         {
-            itemTxts[iterator].text = menu[iterator].name;
-            moneyTxts[iterator].text = "$" + moneys[iterator].ToString();
-            total += moneys[iterator];
+            if (iterator < menu.Count)
+            {
+                itemTxts[iterator].text = menu[iterator].name;
+                moneyTxts[iterator].text = "$" + moneys[iterator].ToString();
+                total += moneys[iterator];
+            }
+            else
+            {
+                itemTxts[iterator].text = "---";
+                moneyTxts[iterator].text = "$0";
+            }
         }
 
         totalTxt.text = "$" + total.ToString();
@@ -30,6 +41,26 @@ public class EndDayReport : MonoBehaviour
         bankTxt.text = "$" + GameManager.instance.bank.ToString();
 
         // trigger animation
+    }
+
+    void SetWeatherIcon(Weather weather)
+    {
+        foreach (var img in weatherIcons) img.SetActive(false);
+        switch (weather)
+        {
+            case Weather.Sun:
+                weatherIcons[0].SetActive(true);
+                break;
+            case Weather.Rain:
+                weatherIcons[1].SetActive(true);
+                break;
+            case Weather.Wind:
+                weatherIcons[2].SetActive(true);
+                break;
+            case Weather.Snow:
+                weatherIcons[3].SetActive(true);
+                break;
+        }
     }
 
     public void NextDay()
